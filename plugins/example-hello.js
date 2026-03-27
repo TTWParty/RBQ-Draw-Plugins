38c5b22cde6fa6abf6a65719cdfd2f1fb54b1f91
TTWP-09 <ttwp-09@TTWP-09deMacBook-Pro.local>
2026-03-27 13:44:34 +0800

    feat: hook support for NAI mode

--- plugins/example-hello.js
+++ plugins/example-hello.js
@@ -1,9 +1,20 @@
 (function(RBQ, $, toastr) {
     if (!RBQ) return console.error('RBQ Core API 缺失!');
+    
     console.info("🌸 Hello from RBQ-Draw HelloWorld Sub-Plugin! 🌸");
+    
+    // 监听白嫖渠道的钩子
     RBQ.on('buildGeneratePayload', (payload) => {
-        toastr.info("Hello World 子插件：我看到了你的绘画请求，已打印到控制台！");
-        console.log("【子插件监控】拦截到的请求参数为: ", payload);
+        toastr.success("Hello World 子插件：拦截到了【白嫖渠道】的绘画请求！");
+        console.log("拦截到的白嫖请求参数为: ", payload);
         return payload; 
     });
+
+    // 监听 NAI 直连模式的钩子
+    RBQ.on('buildNaiV4Payload', (payload) => {
+        toastr.success("Hello World 子插件：拦截到了【NAI】的绘画请求！");
+        console.log("拦截到的 NAI 请求参数为: ", payload);
+        return payload; 
+    });
+
 })(RBQ, jQuery, toastr);

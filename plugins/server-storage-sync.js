6211cecc682f5aff7c3e9b505be3815cd949db3a
TTWP-09 <ttwp-09@TTWP-09deMacBook-Pro.local>
2026-09-21 09:38:30 +0800

    chore: 升级服务端图库同步插件至 v1.1.11 并刷新 plugins.json

--- plugins/server-storage-sync.js
+++ plugins/server-storage-sync.js
@@ -13,7 +13,7 @@
 
     const PLUGIN_ID = 'rbq-gallery-sync';
     const PLUGIN_NAME = '服务端图库同步与存储管理';
-    const PLUGIN_VERSION = '1.1.10';
+    const PLUGIN_VERSION = '1.1.11';
     const STORAGE_KEY = '_gallerySyncSettings';
 
     const DEFAULT_SETTINGS = {
